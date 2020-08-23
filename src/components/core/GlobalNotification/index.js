5101cfb32428a86731da1c1a66f65164c1306ff4
Herlina Sunaryanto <herlina.sunaryanto@sirclo.com>
2020-08-23 14:10:10 +0700

    Global Notification Header

---
+++ src/components/core/GlobalNotification/index.js
@@ -0,0 +1,13 @@
+import theme from './globalnotification.module.scss';
+
+const GlobalNotification = () => {
+    return (
+        <>
+            <div className={theme.globalNotification}>
+                {"This Is Development Site"}
+            </div>
+        </>
+    );
+};
+
+export default GlobalNotification;
--- src/components/core/Layout/index.js
+++ src/components/core/Layout/index.js
@@ -1,11 +1,13 @@
 import Header from '@components/core/Header';
 import Navigation from '@components/core/Navigation';
+import GlobalNotification from '@components/core/GlobalNotification';
 import theme from './layout.module.scss';
 
 const Layout = ({ children }) => {
     return (
         <>
             <div className={theme.wrapper}>
+                <GlobalNotification />
                 <Header />
                 <div className={theme.mainContent}>
                     {children}
