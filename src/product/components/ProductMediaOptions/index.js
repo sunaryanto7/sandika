ab08d6435bd8aabc81733b6a273df936a6872e34
Herlina Sunaryanto <herlina.sunaryanto@sirclo.com>
2020-09-29 08:41:58 +0700

    Swatch Options

--- src/product/components/ProductMediaOptions/index.js
+++ src/product/components/ProductMediaOptions/index.js
@@ -6,31 +6,31 @@ const ProductMediaOptions = ({ options, activeOptions }) => {
 	const [activeAttribute, setActiveAttribute] = useState(activeOptions);
 
 	const sortArrayOfObject = (array) => {
-		const _result = []
+		const _result = [];
 		array.map((unordered) => {
-			let _temp = {}
+			let _temp = {};
 			Object.keys(unordered).sort().forEach((key) => {
-				_temp[key] = unordered[key]
-			})
-			_result.push(_temp)
+				_temp[key] = unordered[key];
+			});
+			_result.push(_temp);
 		})
-		return _result
+		return _result;
 	}
 
 	const activeSwatch = (code) => {
 		let _temp = {};
-		activeAttribute.map((data) => { if (data.code === code) { _temp = data.label } })
+		activeAttribute.map((data) => { if (data.code === code) { _temp = data.label } });
 		return _temp;
 	};
 
 	const handleSetActiveAttribute = (swatch, attribute_code) => {
-		let _temp = { 'code': attribute_code, ...swatch }
-		let _result = []
+		let _temp = { 'code': attribute_code, ...swatch };
+		let _result = [];
 		activeAttribute.filter((data) => {
 			if (data.code === _temp.code) { _temp['__typename'] = "ConfigurableAttributeOption"; _result.push(_temp); }
 			if (data.code !== _temp.code) { _result.push(data) }
-		})
-		setActiveAttribute(sortArrayOfObject(_result))
+		});
+		setActiveAttribute(sortArrayOfObject(_result));
 	};
 
 	return (
@@ -49,8 +49,8 @@ const ProductMediaOptions = ({ options, activeOptions }) => {
 												label={swatch.label}
 												value={JSON.stringify({ ...swatch })}
 
-												name={swatch.label.toLowerCase()}
-												id={swatch.label.toLowerCase()}
+												name={`swatch__opt-${swatch.label.toLowerCase()}`}
+												id={`swatch__opt-${swatch.label.toLowerCase()}`}
 												checked={_temp === swatch.label ? true : false}
 												onClick={() => { handleSetActiveAttribute({ ...swatch }, swatchType.attribute_code) }}
 												key={j} />
