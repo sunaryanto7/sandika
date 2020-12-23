525c51760e7a83a3d1f21c1e395fd8c247aa0375
sunaryanto7 <herlina.sunaryanto@sirclo.com>
2020-12-24 01:02:37 +0700

    add newsletter and copyright

--- pages/_document.js
+++ pages/_document.js
@@ -27,8 +27,8 @@ class SandikaDocument extends Document {
           <meta name="theme-color" content={'#cd134b'} />
           <meta name="msapplication-config" content="/browserconfig.xml" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" type="image/png" href="/media/pwa/favicon-16x16.png" sizes="16x16" />
-          <link rel="icon" type="image/png" href="/media/pwa/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/media/pwa/favicon.ico" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/media/pwa/favicon.ico" sizes="32x32" />
           <link rel="icon" type="image/png" href="/media/pwa/android-192x192.png" sizes="150x150" />
         </Head>
         <body>
